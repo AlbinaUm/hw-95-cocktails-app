7944b32681c18a05c32a2db2eee22f92ac8a49b0
Umarova Albina <albinaumarova2014@gmail.com>
2021-12-15 00:43:18 +0600

    [Backend]: User model, POST/facebookLogin for users.js

---
+++ cocktails-backend/app/users.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const config = require('../config');
+const User = require('../models/User');
+const {nanoid} = require("nanoid");
+const axios = require("axios");
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (e){
+    res.sendStatus(500);
+  }
+});
+
+router.post('/facebookLogin', async (req, res) => {
+  const inputToken = req.body.accessToken;
+
+  const accessToken = config.facebook.appId + '|' + config.facebook.appSecret; //undefined
+
+  const debugTokenURl = `https://graph.facebook.com/debug_token?input_token=${inputToken}&access_token=${accessToken}`;
+
+  try {
+    const response = await axios.get(debugTokenURl);
+
+    if (response.data.data.error){
+      return res.status(401).send({global: 'Facebook token incorrect'});
+    }
+
+    if (req.body.id !== response.data.data.user_id){
+      return res.status(401).send({global: 'Wrong User Id'});
+    }
+
+    let user = await User.findOne({email: req.body.email});
+
+    if (!user) {
+      user = await User.findOne({facebookId: req.body.id});
+    }
+
+    if (!user) {
+      user = new User({
+        email: req.body.email || nanoid() + '@gmail.com',
+        password: nanoid(),
+        facebookId: req.body.id,
+        displayName: req.body.name,
+        avatar: req.body.picture.data.url,
+      });
+    }
+
+    user.generateToken();
+    user.save({validateBeforeSave: false});
+
+    res.send({message: 'Success', user});
+  } catch (e){
+    res.status(401).send({global: 'Facebook token incorrect!'});
+  }
+});
+
+router.delete('/sessions', async (req, res) => {
+  const token = req.get('Authorization');
+  const success = {message: 'Success'};
+
+  if (!token) return res.send(success);
+
+  const user = await User.findOne({token});
+
+  if (!user) return res.send(success);
+
+  user.generateToken();
+
+  await user.save({validateBeforeSave: false});
+
+  return res.send(success);
+});
+
+module.exports = router;
--- cocktails-backend/index.js
+++ cocktails-backend/index.js
@@ -1,1 +1,34 @@
-console.log('Future cocktails API');
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const exitHook = require('async-exit-hook');
+const config = require('./config');
+const users = require('./app/users');
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+
+const port = 8000;
+
+app.use('/users', users);
+
+
+const run = async () => {
+  await mongoose.connect(config.db.url);
+
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+  });
+
+  exitHook(() => {
+    console.log('Exiting');
+    mongoose.disconnect();
+  });
+};
+
+run().catch(e => console.error(e));
